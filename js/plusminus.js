0f4bb72003fefb64645693b15f94dba5f200298c
성백준 <toypiano@naver.com>
2023-08-17 05:35:51 +0900

    plus-minus

---
+++ js/plusminus.js
@@ -0,0 +1,11 @@
+const btnPlus = document.querySelector(".btnPlus");
+const btnMinus = document.querySelector(".btnMinus");
+let num = 0;
+btnPlus.addEventListener("click", e=>{
+  num++
+  console.log(num);
+});
+btnMinus.addEventListener("click", e=>{
+  num--;
+  console.log(num);
+});
