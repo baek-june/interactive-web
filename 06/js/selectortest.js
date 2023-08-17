4aace1ffeb96f59b18e1d47c3677f8010fee5d08
baek-june <toypiano@naver.com>
2023-08-17 11:34:39 +0900

    interactive-web

---
+++ 06/js/selectortest.js
@@ -0,0 +1,10 @@
+const list = document.querySelector(".list");
+const items = list.children;
+console.log(items);
+console.log(items[0]);
+console.log(items[1]);
+console.log(items[2]);
+console.log(items[3]);
+
+const items2 = document.querySelector(".item2");
+console.log(items2.parentElement);
---
+++ 06/js/selectortest3.js
@@ -0,0 +1,3 @@
+const item3 = document.querySelector(".item3");
+console.log(item3.previousElementSibling);
+console.log(item3.nextElementSibling);
