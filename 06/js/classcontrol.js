944ee1422e4b8a273b3d81f667f816481205f880
baek-june <toypiano@naver.com>
2023-08-18 18:37:16 +0900

    toggle

--- 06/js/classcontrol.js
+++ 06/js/classcontrol.js
@@ -1,12 +1,21 @@
 const wrap = document.querySelector("#wrap");
 const box = wrap.querySelector("article");
 
-wrap.addEventListener("click", e=>{
-  let isOn = wrap.classList.contains("on");
-  console.log(isOn);
-  if(isOn) {
-    wrap.classList.remove("on");
-  } else {
-    wrap.classList.add("on");
-  }
+
+// if 조건문
+// wrap.addEventListener("click", ()=>{
+//   let isOn = wrap.classList.contains("on");
+//   console.log(isOn);
+//   (isOn)? wrap.classList.remove("on"): wrap.classList.add("on");
+//   if(isOn) {
+//     wrap.classList.remove("on");
+//   } else {
+//     wrap.classList.add("on");
+//   }
+// });
+
+
+toggle
+wrap.addEventListener("click", ()=>{
+  wrap.classList.toggle("on");
 });
