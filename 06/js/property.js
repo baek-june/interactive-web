9664f20ab5bd7a0ca4b424e7c5c9230db4248b35
baek-june <toypiano@naver.com>
2023-08-18 20:59:46 +0900

    속성값변경

---
+++ 06/js/property.js
@@ -0,0 +1,9 @@
+const link = document.querySelector("a");
+const link_href = link.getAttribute("href");
+console.log(link_href);
+
+// link.addEventListener("click", e=>{
+//   e.preventDefault();
+// });
+
+const new_href = "https://www.nate.com";
